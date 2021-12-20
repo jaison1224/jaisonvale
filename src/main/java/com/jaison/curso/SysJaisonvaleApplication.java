5eae055db2d383c6a4b728d98c0b568b8b4fd217
jaison1224 <jaison1232@gmail.com>
2021-12-19 21:13:24 -0300

    firts commit

---
+++ src/main/java/com/jaison/curso/SysJaisonvaleApplication.java
@@ -0,0 +1,13 @@
+package com.jaison.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysJaisonvaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysJaisonvaleApplication.class, args);
+	}
+
+}
